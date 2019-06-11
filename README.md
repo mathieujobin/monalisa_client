5d47b2af67732cbd3e3044846efcf394f0dc1b20
Mathieu J <mathieujobin@users.noreply.github.com>
2019-06-11 15:46:26 +0900

    initial version

--- README.md
+++ README.md
@@ -1,8 +1,64 @@
 # monalisa_client
 
-A new Flutter package project.
+Small library to authenticate your Flutter up with Monalisa API/SSO
 
-## Getting Started
+## Add to your project
+
+in your pubspec.yaml file
+```yaml
+dependencies:
+  flutter:
+    sdk: flutter
+  monalisa_client:
+    git:
+      url: https://github.com/mathieujobin/monalisa_client.git
+
+assets:
+  - assets/monalisa_config.json
+```
+
+## Create and save your app token
+
+Visit https://monalisa.solidcode.bz/app
+
+## Create Config file
+
+```json
+{
+  "base_url": "https://monalisa.solidcode.bz",
+  "environment": "production",
+  "client_application_name": "rockpaperscissors",
+  "client_application_secret": "1234"
+}
+```
+
+## Basic usage
+
+```dart
+import 'package:monalisa_client/monalisa_client.dart';
+final MonalisaClient monalisa_client = MonalisaClient();
+
+  @override
+  void initState() {
+    super.initState();
+
+    monalisa_client.read_local_config().then((data) {
+      return monalisa_client.ensure_user_token();
+    }).then((data) {
+      setState(() {
+        // this is only to debug execution order,
+        // setState should not be called before ensure_user_token is completed.
+        // also to show how to access user_uuid
+        if (monalisa_client.user_uuid == null) {
+          throw Exception("user_uuid is missing");
+        }
+        _user_logged_in = true;
+      });
+    });
+  }
+```
+
+## Dart/Flutter package resources
 
 This project is a starting point for a Dart
 [package](https://flutter.dev/developing-packages/),
--- lib/monalisa_client.dart
+++ lib/monalisa_client.dart
@@ -1,7 +1,135 @@
 library monalisa_client;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/services.dart' show rootBundle; // rootBundle
+import 'dart:convert' show json; // utf8;
+import 'package:http/http.dart' as http;
+import 'dart:io' show Platform; // http client + Platform
+import 'dart:async'; // Future/async/await;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class MonalisaClient {
+  Map local_config;
+  String user_uuid;
+  String user_token;
+  //final http_client = new HttpClient();
+  FlutterSecureStorage _secure_storage;
+  final http.BaseClient httpClient = http.Client();
+
+  Future<String> load_json_asset(String filename) async {
+    return await rootBundle.loadString('assets/$filename.json');
+  }
+
+  Future<Map> read_local_config() {
+    return load_json_asset('monalisa_config').then((data) {
+      local_config = json.decode(data);
+      return local_config;
+    });
+  }
+
+  Future two_legged_post() {
+
+  }
+
+  /*Future three_legged_get() {
+    http_client.getUrl(Uri.parse())
+        .then((HttpClientRequest request) {
+      return request.close();
+    }).then((HttpClientResponse response) {
+    });
+  }*/
+
+  Future<bool> ensure_user_token() async {
+    user_uuid = await stored_user_uuid;
+    user_token = await stored_user_token;
+    if (user_uuid == null || user_uuid.length != 36 || user_token == null || user_token.length != 36) {
+      return token_create();
+    } else {
+      return token_verify();
+    }
+  }
+
+  Future<bool> token_create() {
+    try {
+      return httpClient.post(token_create_url, headers: two_legged_auth_headers).then((res) {
+        if (res.statusCode == 201) {
+          Map response_map = json.decode(res.body);
+          user_uuid = response_map['user_uuid'];
+          user_token = response_map['secret_token'];
+          secure_storage.write(key: 'user_uuid', value: response_map['user_uuid']);
+          secure_storage.write(key: 'secret_token', value: response_map['secret_token']);
+          return true;
+        } else {
+          return false;
+        }
+      });
+    } catch(e, _) {
+      var completer = new Completer();
+      completer.complete(false);
+      return completer.future;
+    }
+  }
+
+  Future<bool> token_verify() {
+    return httpClient.get(token_verify_url, headers: three_legged_auth_headers).then((res) {
+      return res.statusCode == 200;
+    });
+  }
+
+  String get token_create_url {
+    return local_config["base_url"]+"/api/v1/token";
+  }
+
+  String get token_verify_url {
+    return local_config["base_url"]+"/api/v1/token/verify";
+  }
+
+  Map<String, String> get two_legged_auth_headers {
+    return {
+      'X-SC-CLIENT-PLATFORM': client_platform,
+      'X-SC-CLIENT-LOCALE': Platform.localeName,
+      'X-SC-CLIENT-VERSION': Platform.version,
+      'X-SC-CLIENT-OS-VERSION': Platform.operatingSystemVersion,
+      'X-SC-APP-NAME': app_name,
+      'X-SC-APP-SECRET': app_secret
+    };
+  }
+
+  Map<String, String> get three_legged_auth_headers {
+    return {
+      'X-SC-APP-NAME': app_name,
+      'X-SC-APP-SECRET': app_secret,
+      'X-SC-USER-UUID': user_uuid,
+      'X-SC-USER-TOKEN': user_token
+    };
+  }
+
+  String get app_name {
+    return local_config["client_application_name"];
+  }
+
+  String get app_secret {
+    return local_config["client_application_secret"];
+  }
+
+  Future<String> get stored_user_uuid {
+    return secure_storage.read(key: 'user_uuid');
+  }
+
+  Future<String> get stored_user_token {
+    return secure_storage.read(key: 'secret_token');
+  }
+
+  FlutterSecureStorage get secure_storage {
+    if (_secure_storage == null)
+      this._secure_storage = new FlutterSecureStorage();
+    return _secure_storage;
+  }
+
+  String get client_platform {
+    String os = Platform.operatingSystem;
+    if (os != "ios" && os != "android") {
+      throw new Exception("unsupported platform $os");
+    }
+    return os;
+  }
 }
